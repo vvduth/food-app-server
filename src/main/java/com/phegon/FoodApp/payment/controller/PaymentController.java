747b57e2014e248521314b50b28fbe371e063ee7
vvduth <ducthai060501@gmail.com>
2025-07-30 17:02:01 +0300

    add PaymentController for managing payments, including initiating payments, updating payment status, and fetching payment details

---
+++ src/main/java/com/phegon/FoodApp/payment/controller/PaymentController.java
@@ -0,0 +1,44 @@
+package com.phegon.FoodApp.payment.controller;
+
+import com.phegon.FoodApp.payment.dtos.PaymentDTO;
+import com.phegon.FoodApp.payment.services.PaymentService;
+import com.phegon.FoodApp.response.Response;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/pay")
+    public ResponseEntity<Response<?>> initializePayment(@RequestBody @Valid PaymentDTO paymentRequest) {
+        return ResponseEntity.ok(paymentService.initiatePayment(paymentRequest));
+    }
+
+    @PutMapping("/update")
+    public void updatePaymentForOrder(@RequestBody  PaymentDTO paymentRequest){
+        paymentService.updatePaymentForOrder(paymentRequest);
+    }
+
+    @PostMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response<List<PaymentDTO>>> getAllPayments() {
+        Response<List<PaymentDTO>> response = paymentService.getAllPayments();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{paymentId}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response<PaymentDTO>> getPaymentById(@PathVariable Long paymentId) {
+        Response<PaymentDTO> response = paymentService.getPaymentById(paymentId);
+        return ResponseEntity.ok(response);
+    }
+}
